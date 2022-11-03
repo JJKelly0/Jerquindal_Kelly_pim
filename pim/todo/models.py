25245d3df72fb10ee9d0390926ca8dd1b5f144be
JJ Kelly <88286005+JJKelly0@users.noreply.github.com>
2022-11-02 19:26:27 -0500

    slow and steady

--- pim/todo/models.py
+++ pim/todo/models.py
@@ -1,8 +1,27 @@
+from django.core.mail import send_mail
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default = "", blank=True)
     due_date = models.DateTimeField(null=True, blank=True)
     complete_date = models.DateTimeField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    notify = models.EmailField(max_length=255, default = "", blank=True)
+
+    def __str__(self):
+        return self.name
+
+@receiver(post_save, sender=Task)
+def send_email_to_interested_party(sender, instance, created, **kwargs):
+    if instance.complete:
+        send_mail(
+            'Subject here',
+            'Here is the message'
+            'from@example.com'
+            [instance.notify, ],
+            fail_silently=False,
+        )
