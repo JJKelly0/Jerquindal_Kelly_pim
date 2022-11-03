546d536fd0a68d2017a8b1387d4143381d78bf85
JJ Kelly <88286005+JJKelly0@users.noreply.github.com>
2022-11-02 20:32:47 -0500

    image functionality

--- pim/todo/models.py
+++ pim/todo/models.py
@@ -1,27 +1,33 @@
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 # Create your models here.
 
+def send_an_email():
+    print("Sending an email")
+
+
 class Task(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(default = "", blank=True)
+    objects = None
+    name = models.CharField(max_length=255, default="")
+    description = models.TextField(default="", blank=True)
     due_date = models.DateTimeField(null=True, blank=True)
     complete_date = models.DateTimeField(null=True, blank=True)
     complete = models.BooleanField(default=False)
-    notify = models.EmailField(max_length=255, default = "", blank=True)
+    notify = models.EmailField(max_length=255, default="", blank=True)
+    category = models.CharField(max_length=45, default="", choices=(('IMPORTANT', 'IMPORTANT'), ('URGENT', 'URGENT'), ('NORMAL', 'NORMAL')))
+    priority = models.CharField(max_length=45, default="", choices=(('PRIORITY', 'PRIORITY'), ('NON-PRIORITY', 'NON-PRIORITY')))
+    task_image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-@receiver(post_save, sender=Task)
-def send_email_to_interested_party(sender, instance, created, **kwargs):
-    if instance.complete:
-        send_mail(
-            'Subject here',
-            'Here is the message'
-            'from@example.com'
-            [instance.notify, ],
-            fail_silently=False,
-        )
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.complete and self.notify:
+            send_an_email()
+
+# @receiver(post_save, sender=Task)
+# def send_email_to_interested_party(sender, instance, created, **kwargs):
+#   if instance.complete:
+#      print('Subject here,\n Here is the message\n from@example.com')
--- pim/todo/tests.py
+++ pim/todo/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from unittest.mock import patch  # , Mock
 
-# Create your tests here.
+from pim.todo.models import Task
+
+
+class TestSendingEmail(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @patch('models.send_an_email')
+    def test_sending_email_on_complete(self, mock_send_email):
+        task = Task.objects.create(name="First", notify="foo@bar.com")
+        task.Complete = True
+        task.save()
+        assert mock_send_email.called
+
+    @patch('models.send_an_email')
+    def test_not_sending_email_on_complete(self, mock_send_email):
+        task = Task.objects.create(name="First", notify="foo@bar.com")
+        task.Complete = True
+        task.save()
+        assert not mock_send_email.called
